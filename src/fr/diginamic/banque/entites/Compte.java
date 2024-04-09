c0a9f50fc96d08568c2e3015fbb53df0ab4a88e1
LerouvilloisValentin <lerouvillois.valentin61@hotmail.fr>
2024-04-09 12:29:52 +0200

    tp5 java orienté object

---
+++ src/fr/diginamic/banque/entites/Compte.java
@@ -0,0 +1,22 @@
+package fr.diginamic.banque.entites;
+
+public class Compte {
+
+	int numCompte;
+	int solde;
+	
+	Compte(int numCompte, int solde){
+		this.numCompte = numCompte;
+		this.solde = solde;
+	}
+	
+	
+	
+	
+	
+	public static void main(String[] args) {
+	
+
+	}
+
+}
---
+++ src/fr/diginamic/banque/entites/TestBanque.java
@@ -0,0 +1,13 @@
+package fr.diginamic.banque.entites;
+
+
+public class TestBanque {
+
+	
+	public static void main(String[] args) {
+		Compte compte = new Compte(115,245);
+		System.out.println(compte);
+		
+	}
+
+}
